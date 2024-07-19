fdc57f137b15a1d4737f92b67156508e03108d76
iocods <Iocodes2000@gmail.com>
2024-07-19 13:20:35 +0100

    Applied styling to the message page

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,15 +1,55 @@
 import React from 'react'
 
 const Messages = () => {
-  return (
-        <>
-            <nav>
-                <h2>Chats</h2>
+    return (
+        <div className='home-div-1 message-page'>
+            <nav className='threads'>
+                <h2>Threads</h2>
+                <ul>
+                    <li><button>Women's Fashion</button></li>
+                    <li><button>Men's Fashion</button></li>
+                    <li><button>Electronics</button></li>
+                    <li><button>Home & LifeStyle</button></li>
+                    <li><button>Airtime</button></li>
+                    <li><button>Confectioneries</button></li>
+                    <li><button>Stationery</button></li>
+                    <li><button>Groceries</button></li>
+                    <li><button>Beauty</button></li>
+                </ul>
             </nav>
-            <div>
-                <></>
+            <div className='message-body'>
+                <div className='sender'>
+                    <p>
+                        Hello Good day I see that you're willing to make a purchase I would love to know how many it is that you'll love to buy
+                    </p>
+                </div>
+                <div className='receiver'>
+                    <p>
+                        Hello Good day I see that you're willing to make a purchase I would love to know how many it is that you'll love to buy
+                    </p>
+                </div>
+                <div className='sender'>
+                    <p>
+                        Hello Good day I see that you're willing to make a purchase I would love to know how many it is that you'll love to buy
+                    </p>
+                </div>
+                <div className='receiver'>
+                    <p>
+                        Hello Good day I see that you're willing to make a purchase I would love to know how many it is that you'll love to buy
+                    </p>
+                </div>
+                <div className='sender'>
+                    <p>
+                        Hello Good day I see that you're willing to make a purchase I would love to know how many it is that you'll love to buy
+                    </p>
+                </div>
+                <div className='receiver'>
+                    <p>
+                        Hello Good day I see that you're willing to make a purchase I would love to know how many it is that you'll love to buy
+                    </p>
+                </div>
             </div>
-        </>
+        </div>
     )
 }
 
